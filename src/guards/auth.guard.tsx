456657b526f0bc25643f6dace31862d79a0b5cbb
neominal.solutions <neominal.solutions@gmail.com>
2025-06-13 14:48:33 +0300

    Guard ile Sayfaların Routelarının korunması

--- src/guards/auth.guard.tsx
+++ src/guards/auth.guard.tsx
@@ -1,5 +1,6 @@
 // Eğer korumalaı sayfalar varsa bu durumda kullanıcın oturum açıp açmamasına göre yada yetkisinin olup olmamasına göre sayfanın açılıp açılmayacağına karar vermek için react router da navigate componeti kullanarak bir yapı kurarız. Bu tarz yapılara guard ismini veriyoruz. Route Guard
 
+import { jwtDecode } from 'jwt-decode';
 import { Navigate } from 'react-router';
 
 type AuthProps = {
@@ -12,9 +13,17 @@ function AuthGuard({ children }: AuthProps) {
 	console.log('token', token);
 	console.log('...AuthGuard');
 
-	if (token) {
-		return <>{children}</>;
-	} else {
+	try {
+		if (token) {
+			const jwt = jwtDecode(token as string);
+			console.log('jwt', jwt);
+
+			return <>{children}</>;
+		} else {
+			return <Navigate to="/unauthorized" />;
+		}
+	} catch (error) {
+		console.log('err', error);
 		return <Navigate to="/unauthorized" />;
 	}
 }
